b2e5e20aec4998b226877cf82f808ac085f6c9c7
al.lee <al.lee@linecorp.com>
2017-08-01 16:54:13 +0900

    update kt

--- kt.rb
+++ kt.rb
@@ -1,8 +1,8 @@
 class Kt < Formula
   desc "Select a pod and tailing multiple k8s pods logs at the same time"
   homepage "https://github.com/leoh0/kt"
-  url "https://github.com/leoh0/kt/archive/leoh0/1.0.1.tar.gz"
-  sha256 "d8bbbf1599778fb6a1043c66e60ee59713c9773a85d18b6cc4d54df427a5e56a"
+  url "https://github.com/leoh0/kt/archive/leoh0/1.0.2.tar.gz"
+  sha256 "080601857ad6d7a368f7062d819adad277b5d46cebf4d39e77f43136196c0eb4"
   head "https://github.com/leoh0/kt.git"
 
   def install
